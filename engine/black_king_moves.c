d4f8f2fedae48157c136962adeec623acc8505ef
andreatc <andreatc@ntnu.no>
2024-12-18 18:11:27 +0100

    Added move generation for queen and king moves

---
+++ engine/black_king_moves.c
@@ -0,0 +1,27 @@
+#include "piece_moves.h"
+
+static inline void generate_black_king_moves(BoardState *board_state, uint8_t x, uint8_t y, BoardStack *stack)
+{
+    for (int dy = -1; dy <= 1; dy++)
+    {
+        for (int dx = -1; dx <= 1; dx++)
+        {
+            if (dx == 0 && dy == 0)
+                continue;
+            if (x + dx >= 0 && x + dx < 8 && y + dy >= 0 && y + dy < 8)
+            {
+                if (board_state->white_pieces & position_to_u64(x + dx, y + dy))
+                {
+                    BoardState *new_board_state = &stack->boards[stack->count];
+                    copy_board(&board_state->board, &new_board_state->board);
+                    remove_white_piece(new_board_state, x + dx, y + dy);
+                    new_board_state->board.black_pieces.king &= ~position_to_u64(x, y);
+                    new_board_state->board.black_pieces.king |= position_to_u64(x + dx, y + dy);
+                    new_board_state->board.side_to_move = WHITE;
+                    init_board(new_board_state);
+                    validate_black_move(stack);
+                }
+            }
+        }
+    }
+}
---
+++ engine/black_queen_moves.c
@@ -0,0 +1,205 @@
+#include "piece_moves.h"
+
+static inline void generate_black_queen_moves(BoardState *board_state, uint8_t x, uint8_t y, BoardStack *stack)
+{
+    for (uint8_t i = x + 1; i < 8; i++)
+    {
+        if (board_state->occupied & position_to_u64(i, y))
+        {
+            if (board_state->white_pieces & position_to_u64(i, y))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, i, y);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(i, y);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(i, y);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = x - 1; i < 8; i--)
+    {
+        if (board_state->occupied & position_to_u64(i, y))
+        {
+            if (board_state->white_pieces & position_to_u64(i, y))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, i, y);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(i, y);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(i, y);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = y + 1; i < 8; i++)
+    {
+        if (board_state->occupied & position_to_u64(x, i))
+        {
+            if (board_state->white_pieces & position_to_u64(x, i))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, x, i);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(x, i);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(x, i);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = y - 1; i < 8; i--)
+    {
+        if (board_state->occupied & position_to_u64(x, i))
+        {
+            if (board_state->white_pieces & position_to_u64(x, i))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, x, i);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(x, i);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(x, i);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = 1; x + i < 8 && y + i < 8; i++)
+    {
+        if (board_state->occupied & position_to_u64(x + i, y + i))
+        {
+            if (board_state->white_pieces & position_to_u64(x + i, y + i))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, x + i, y + i);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(x + i, y + i);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(x + i, y + i);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = 1; x + i < 8 && y - i < 8; i++)
+    {
+        if (board_state->occupied & position_to_u64(x + i, y - i))
+        {
+            if (board_state->white_pieces & position_to_u64(x + i, y - i))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, x + i, y - i);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(x + i, y - i);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(x + i, y - i);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = 1; x - i < 8 && y + i < 8; i++)
+    {
+        if (board_state->occupied & position_to_u64(x - i, y + i))
+        {
+            if (board_state->white_pieces & position_to_u64(x - i, y + i))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, x - i, y + i);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(x - i, y + i);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(x - i, y + i);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+    for (uint8_t i = 1; x - i < 8 && y - i < 8; i++)
+    {
+        if (board_state->occupied & position_to_u64(x - i, y - i))
+        {
+            if (board_state->white_pieces & position_to_u64(x - i, y - i))
+            {
+                BoardState *new_board_state = &stack->boards[stack->count];
+                copy_board(&board_state->board, &new_board_state->board);
+                remove_white_piece(new_board_state, x - i, y - i);
+                new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+                new_board_state->board.black_pieces.queens |= position_to_u64(x - i, y - i);
+                new_board_state->board.side_to_move = WHITE;
+                init_board(new_board_state);
+                validate_black_move(stack);
+            }
+            break;
+        }
+        BoardState *new_board_state = &stack->boards[stack->count];
+        copy_board(&board_state->board, &new_board_state->board);
+        new_board_state->board.black_pieces.queens &= ~position_to_u64(x, y);
+        new_board_state->board.black_pieces.queens |= position_to_u64(x - i, y - i);
+        new_board_state->board.side_to_move = WHITE;
+        init_board(new_board_state);
+        validate_black_move(stack);
+    }
+}
