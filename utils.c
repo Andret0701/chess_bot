9963f71ebfff55803dc97b500e10d76e1517e0a0
andreatc <andreatc@ntnu.no>
2024-12-18 14:16:46 +0100

    Added popcountll and ctzll

---
+++ utils.c
@@ -0,0 +1,29 @@
+#include "utils.h"
+
+// Count the number of 1 bits in an unsigned long long integer
+uint8_t popcountll(uint64_t x)
+{
+    x = x - ((x >> 1) & 0x5555555555555555ULL);
+    x = (x & 0x3333333333333333ULL) + ((x >> 2) & 0x3333333333333333ULL);
+    x = (x + (x >> 4)) & 0x0F0F0F0F0F0F0F0FULL;
+    x = x + (x >> 8);
+    x = x + (x >> 16);
+    x = x + (x >> 32);
+    return x & 0x7F;
+}
+
+// Count trailing zeros in an unsigned long long integer
+uint8_t ctzll(uint64_t x)
+{
+    if (x == 0)
+        return 64;
+
+    uint8_t count = 0;
+    while ((x & 1) == 0)
+    {
+        x >>= 1;
+        count++;
+    }
+
+    return count;
+}
---
+++ utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+
+uint8_t popcountll(uint64_t x);
+uint8_t ctzll(uint64_t x);
