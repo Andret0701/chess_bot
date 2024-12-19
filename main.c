b545878693d87c677a09495456aca3b414891856
andreatc <andreatc@ntnu.no>
2024-12-19 15:00:51 +0100

    Updated main to be count_test

--- main.c
+++ main.c
@@ -4,16 +4,10 @@
 #include "engine/piece_moves.h"
 #include "fen.h"
 
+#include "engine/tests/count_test.h"
+
 int main()
 {
-    BoardStack *stack = create_board_stack(1000);
-    Board board = fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-
-    stack->count = 0;
-    generate_moves(&board, stack);
-
-    printf("Number of moves: %d\n", stack->count);
-
-    destroy_board_stack(stack);
+    run_count_tests();
     return 0;
 }
