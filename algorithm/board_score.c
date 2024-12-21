e2f9588fbcdf0d654c35f01e47594740a24eec62
andreatc <andreatc@ntnu.no>
2024-12-21 21:18:52 +0100

    Added system for comparing board scores

---
+++ algorithm/board_score.c
@@ -0,0 +1,123 @@
+#include "board_score.h"
+
+bool has_won(Result result, Color color)
+{
+    return (result == WHITE_WON && color == WHITE) || (result == BLACK_WON && color == BLACK);
+}
+
+bool has_lost(Result result, Color color)
+{
+    return (result == BLACK_WON && color == WHITE) || (result == WHITE_WON && color == BLACK);
+}
+
+BoardScore get_worst_score(Color color)
+{
+    BoardScore score;
+    score.score = color == WHITE ? INT32_MIN : INT32_MAX;
+    score.result = color == WHITE ? BLACK_WON : WHITE_WON;
+    score.depth = 0;
+    return score;
+}
+
+bool is_better_score(BoardScore a, BoardScore b, Color color)
+{
+    if (color == WHITE)
+    {
+        if (a.result == b.result)
+            return a.score > b.score;
+
+        if (a.result == WHITE_WON)
+            return true;
+
+        if (b.result == WHITE_WON)
+            return false;
+
+        if (a.result == UNKNOWN)
+            return true;
+
+        if (b.result == UNKNOWN)
+            return false;
+
+        if (a.result == DRAW)
+            return true;
+
+        return false;
+    }
+    else
+    {
+        if (a.result == b.result)
+            return a.score < b.score;
+
+        if (a.result == BLACK_WON)
+            return true;
+
+        if (b.result == BLACK_WON)
+            return false;
+
+        if (a.result == UNKNOWN)
+            return true;
+
+        if (b.result == UNKNOWN)
+            return false;
+
+        if (a.result == DRAW)
+            return true;
+
+        return false;
+    }
+}
+
+bool is_better_equal(BoardScore a, BoardScore b, Color color)
+{
+    if (color == WHITE)
+    {
+        if (a.result == b.result)
+            return a.score >= b.score;
+
+        if (a.result == WHITE_WON)
+            return true;
+
+        if (b.result == WHITE_WON)
+            return false;
+
+        if (a.result == UNKNOWN)
+            return true;
+
+        if (b.result == UNKNOWN)
+            return false;
+
+        if (a.result == DRAW)
+            return true;
+
+        return false;
+    }
+    else
+    {
+        if (a.result == b.result)
+            return a.score <= b.score;
+
+        if (a.result == BLACK_WON)
+            return true;
+
+        if (b.result == BLACK_WON)
+            return false;
+
+        if (a.result == UNKNOWN)
+            return true;
+
+        if (b.result == UNKNOWN)
+            return false;
+
+        if (a.result == DRAW)
+            return true;
+
+        return false;
+    }
+}
+
+BoardScore max_score(BoardScore a, BoardScore b, Color color)
+{
+    if (is_better_score(a, b, color))
+        return a;
+    return b;
+}
---
+++ algorithm/board_score.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "board.h"
+#include <stdint.h>
+
+typedef enum
+{
+    WHITE_WON,
+    BLACK_WON,
+    DRAW,
+    UNKNOWN
+} Result;
+
+
+typedef struct
+{
+    int32_t score;
+    Result result;
+    uint8_t depth;
+} BoardScore;
+
+bool has_won(Result result, Color color);
+bool has_lost(Result result, Color color);
+BoardScore get_worst_score(Color color);
+
+bool is_better_score(BoardScore a, BoardScore b, Color color);
+bool is_better_equal(BoardScore a, BoardScore b, Color color);
+BoardScore max_score(BoardScore a, BoardScore b, Color color);
