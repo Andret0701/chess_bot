8aac38199e920a0b5c028715271290b62c3cc321
andreatc <andreatc@ntnu.no>
2024-12-31 13:14:27 +0100

    Reworked board score to include depth

--- algorithm/board_score.c
+++ algorithm/board_score.c
@@ -57,22 +57,74 @@ bool is_better_result(Result a, Result b, Color color, Color bot_color)
     return a_index < b_index;
 }
 
-bool is_better_score(BoardScore a, BoardScore b, Color color, Color bot_color) // this should use depth. draw should be desired if loosing
+bool is_better_score(BoardScore a, BoardScore b, Color color) // this should use depth. draw should be desired if loosing
 {
-    if (color == WHITE)
-    {
-        if (a.result == b.result)
-            return a.score > b.score;
+    bool has_better_score = color == WHITE ? a.score >= b.score : a.score <= b.score;
+    Result win_result = color == WHITE ? WHITE_WON : BLACK_WON;
+    Result lost_result = color == WHITE ? BLACK_WON : WHITE_WON;
 
-        return is_better_result(a.result, b.result, color, bot_color);
-    }
-    else
+    if (a.result == win_result && b.result == win_result)
     {
-        if (a.result == b.result)
-            return a.score < b.score;
-
-        return is_better_result(a.result, b.result, color, bot_color);
+        if (a.depth == b.depth)
+            return has_better_score;
+        return a.depth < b.depth;
     }
+    else if (a.result == win_result && (b.result == UNKNOWN || b.result == DRAW || b.result == lost_result))
+        return true;
+    else if ((a.result == UNKNOWN || a.result == DRAW || a.result == lost_result) && b.result == win_result)
+        return false;
+    else if (a.result == UNKNOWN && b.result == UNKNOWN)
+        return has_better_score;
+    else if (a.result == UNKNOWN && b.result == DRAW)
+    {
+        bool a_is_winning = color == WHITE ? a.score >= 0 : a.score <= 0;
+        if (a_is_winning)
+            return true;
+        return false;
+    }
+    else if (a.result == DRAW && b.result == UNKNOWN)
+    {
+        bool b_is_winning = color == WHITE ? b.score >= 0 : b.score <= 0;
+        if (b_is_winning)
+            return false;
+        return true;
+    }
+    else if (a.result == UNKNOWN && b.result == lost_result)
+        return true;
+    else if (a.result == lost_result && b.result == UNKNOWN)
+        return false;
+    else if (a.result == DRAW && b.result == DRAW)
+    {
+        bool a_is_loosing = color == WHITE ? a.score < 0 : a.score > 0;
+        bool b_is_loosing = color == WHITE ? b.score < 0 : b.score > 0;
+        bool both_is_loosing = a_is_loosing && b_is_loosing;
+        if (both_is_loosing && a.depth < b.depth)
+            return true;
+
+        bool a_is_winning = color == WHITE ? a.score >= 0 : a.score <= 0;
+        bool b_is_winning = color == WHITE ? b.score >= 0 : b.score <= 0;
+        bool both_is_winning = a_is_winning && b_is_winning;
+        if (both_is_winning && a.depth > b.depth)
+            return true;
+
+        return has_better_score;
+    }
+    else if (a.result == DRAW && b.result == lost_result)
+        return true;
+    else if (a.result == lost_result && b.result == DRAW)
+        return false;
+
+    // else if (a.result == lost_result && b.result == lost_result)
+    // {
+    if (a.depth == b.depth)
+        return has_better_score;
+    return a.depth > b.depth;
+    // }
+
+    // w-w w-u w-d w-l
+    // u-w u-u u-d u-l
+    // d-w d-u d-d d-l
+    // l-w l-u l-d l-l
 }
 
 bool is_better_equal(BoardScore a, BoardScore b, Color color, Color bot_color)
