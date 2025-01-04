fa0962e8ed2c8cb52c01da7e80940bb63b4b67fd
andreatc <andreatc@ntnu.no>
2025-01-05 00:06:50 +0100

    Optimized attack generation

--- engine/attack_generation/attack_generation.c
+++ engine/attack_generation/attack_generation.c
@@ -7,19 +7,22 @@ uint64_t generate_white_attacks(Board *board)
     attacks |= generate_knight_attacks(board->white_pieces.knights);
     attacks |= generate_king_attacks(board->white_pieces.king);
     uint64_t white_sliding_pieces = board->white_pieces.bishops | board->white_pieces.rooks | board->white_pieces.queens;
-    for (uint8_t i = 0; i < 64; i++)
+
+    while (white_sliding_pieces)
     {
-        uint64_t position = 1ULL << i;
-        if (white_sliding_pieces & position)
-        {
-            if (board->white_pieces.bishops & position)
-                attacks |= generate_bishop_attacks(occupied, i);
-            else if (board->white_pieces.rooks & position)
-                attacks |= generate_rook_attacks(occupied, i);
-            else
-                attacks |= generate_queen_attacks(occupied, i);
-        }
+        int index = __builtin_ctzll(white_sliding_pieces);
+        white_sliding_pieces &= white_sliding_pieces - 1;
+
+        uint64_t position = 1ULL << index;
+
+        if (board->white_pieces.bishops & position)
+            attacks |= generate_bishop_attacks(occupied, index);
+        else if (board->white_pieces.rooks & position)
+            attacks |= generate_rook_attacks(occupied, index);
+        else
+            attacks |= generate_queen_attacks(occupied, index);
     }
+
     return attacks;
 }
 
@@ -30,19 +33,22 @@ uint64_t generate_black_attacks(Board *board)
     attacks |= generate_knight_attacks(board->black_pieces.knights);
     attacks |= generate_king_attacks(board->black_pieces.king);
     uint64_t black_sliding_pieces = board->black_pieces.bishops | board->black_pieces.rooks | board->black_pieces.queens;
-    for (uint8_t i = 0; i < 64; i++)
+
+    while (black_sliding_pieces)
     {
-        uint64_t position = 1ULL << i;
-        if (black_sliding_pieces & position)
-        {
-            if (board->black_pieces.bishops & position)
-                attacks |= generate_bishop_attacks(occupied, i);
-            else if (board->black_pieces.rooks & position)
-                attacks |= generate_rook_attacks(occupied, i);
-            else
-                attacks |= generate_queen_attacks(occupied, i);
-        }
+        int index = __builtin_ctzll(black_sliding_pieces);
+        black_sliding_pieces &= black_sliding_pieces - 1;
+
+        uint64_t position = 1ULL << index;
+
+        if (board->black_pieces.bishops & position)
+            attacks |= generate_bishop_attacks(occupied, index);
+        else if (board->black_pieces.rooks & position)
+            attacks |= generate_rook_attacks(occupied, index);
+        else
+            attacks |= generate_queen_attacks(occupied, index);
     }
+
     return attacks;
 }
 
@@ -66,27 +72,37 @@ void generate_attacks(BoardState *board_state)
     uint64_t white_sliding_pieces = board_state->board.white_pieces.bishops | board_state->board.white_pieces.rooks | board_state->board.white_pieces.queens;
     uint64_t black_sliding_pieces = board_state->board.black_pieces.bishops | board_state->board.black_pieces.rooks | board_state->board.black_pieces.queens;
 
-    for (uint8_t i = 0; i < 64; i++)
+    // Process white pieces
+    while (white_sliding_pieces)
     {
-        uint64_t position = 1ULL << i;
-        if (white_sliding_pieces & position)
-        {
-            if (board_state->board.white_pieces.bishops & position)
-                board_state->white_attacks.bishops |= generate_bishop_attacks(board_state->occupied, i);
-            else if (board_state->board.white_pieces.rooks & position)
-                board_state->white_attacks.rooks |= generate_rook_attacks(board_state->occupied, i);
-            else
-                board_state->white_attacks.queens |= generate_queen_attacks(board_state->occupied, i);
-        }
-        else if (black_sliding_pieces & position)
-        {
-            if (board_state->board.black_pieces.bishops & position)
-                board_state->black_attacks.bishops |= generate_bishop_attacks(board_state->occupied, i);
-            else if (board_state->board.black_pieces.rooks & position)
-                board_state->black_attacks.rooks |= generate_rook_attacks(board_state->occupied, i);
-            else
-                board_state->black_attacks.queens |= generate_queen_attacks(board_state->occupied, i);
-        }
+        // Get least significant set bit index and clear it
+        int index = __builtin_ctzll(white_sliding_pieces);
+        white_sliding_pieces &= white_sliding_pieces - 1;
+
+        uint64_t position = 1ULL << index;
+
+        if (board_state->board.white_pieces.bishops & position)
+            board_state->white_attacks.bishops |= generate_bishop_attacks(board_state->occupied, index);
+        else if (board_state->board.white_pieces.rooks & position)
+            board_state->white_attacks.rooks |= generate_rook_attacks(board_state->occupied, index);
+        else
+            board_state->white_attacks.queens |= generate_queen_attacks(board_state->occupied, index);
+    }
+
+    // Process black pieces
+    while (black_sliding_pieces)
+    {
+        int index = __builtin_ctzll(black_sliding_pieces);
+        black_sliding_pieces &= black_sliding_pieces - 1;
+
+        uint64_t position = 1ULL << index;
+
+        if (board_state->board.black_pieces.bishops & position)
+            board_state->black_attacks.bishops |= generate_bishop_attacks(board_state->occupied, index);
+        else if (board_state->board.black_pieces.rooks & position)
+            board_state->black_attacks.rooks |= generate_rook_attacks(board_state->occupied, index);
+        else
+            board_state->black_attacks.queens |= generate_queen_attacks(board_state->occupied, index);
     }
 
     board_state->white_attack = board_state->white_attacks.pawns | board_state->white_attacks.knights | board_state->white_attacks.bishops | board_state->white_attacks.rooks | board_state->white_attacks.queens | board_state->white_attacks.king;
